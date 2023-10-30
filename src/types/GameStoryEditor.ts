f1f7ba14f4ea4453399a0dc144e26bada8e433fb
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-10-31 00:33:25 +0900

    部分类型定义

--- src/types/GameStoryEditor.ts
+++ src/types/GameStoryEditor.ts
@@ -1,7 +1,23 @@
+export interface Character {
+
+}
+
 export interface StoryEditorTextUnit {
-
+  type:
+    | "title"
+    | "place"
+    | "text"
+    | "option"
+    | "st"
+    | "effectOnly"
+    | "continue"
+  backgroundImage: string;
+  bgmId: number;
+  speaker: string;
+  affiliation: string;
+  text: string;
+  characters: Character[];
+  environmentFx: "";
 }
 
-export interface StoryEditorSelectionUnit {
-  
-}
+export interface StoryEditorSelectionUnit {}
