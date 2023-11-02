9f8b00bc32d01d2d35fc392d57d49cbdf0c1e619
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-11-02 18:40:41 +0900

    补充一下剧情单元类型

--- src/types/GameStoryEditor.ts
+++ src/types/GameStoryEditor.ts
@@ -1,23 +1,62 @@
+import { NumericRange, CreateArrayWithLengthX, TimeStamp } from "./Helper";
+
 export interface Character {
-  
+  name: string;
+  position: 1 | 2 | 3 | 4 | 5;
+  face: string;
+  action:
+  | "heart"
+  | "respond"
+  | "music"
+  | "twinkle"
+  | "upset"
+  | "sweat"
+  | "dot"
+  | "chat"
+  | "exclaim"
+  | "surprise"
+  | "question"
+  | "shy"
+  | "angry"
+  | "steam"
+  | "sigh"
+  | "sad"
+  | "bulb"
+  | "zzz"
+  | "tear"
+  | "disappear"
+  | "hide"
+  filter:
+  | "signal"  // 全息特效
 }
 
 export interface StoryEditorTextUnit {
+  id: TimeStamp;     // 直接用时间戳，不用自增
   type:
-    | "title"
-    | "place"
-    | "text"
-    | "option"
-    | "st"
-    | "effectOnly"
-    | "continue"
+  | "title"       // 标题
+  | "place"       // 场景 banner
+  | "text"        // 文字
+  | "select"      // 选项触发
+  | "option"      // 选项
+  | "st"          // 浮动文字
+  | "effectOnly"  // 仅特效
+  | "continue"    // 显示待续画面
   backgroundImage: string;
   bgmId: number;
-  speaker: string;
-  affiliation: string;
-  text: string;
-  characters: Character[];
-  environmentFx: "";
+  speaker?: string;
+  affiliation?: string;
+  selectionGroup?: NumericRange<CreateArrayWithLengthX<1>, 99>; // 1-99 的数字
+  text?: string;
+  characters?: Character[];
+  command?:
+  | "smoke"
+  | "shake"
+  | "wait"
+  // 还能扩展
+  commandArgs?: (number | string)[];
 }
 
-export interface StoryEditorSelectionUnit {}
+export interface StoryEditorSelectionUnit extends StoryEditorTextUnit {
+  selectionGroup: NumericRange<CreateArrayWithLengthX<1>, 99>;
+  type: "option";
+}
