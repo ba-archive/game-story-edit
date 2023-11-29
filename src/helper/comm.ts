b902051383ca15e472c3c0110f48ace5a018aa41
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-11-30 00:01:50 +0900

    API

--- src/helper/comm.ts
+++ src/helper/comm.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Story } from "@/types/GameStoryEditor.ts";
 
 const base = "https://api.blue-archive.io";
 
@@ -8,12 +9,33 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  timeoutErrorMessage: "请求超时",
 });
 
+// 获取列表
 export async function getStoryList() {
-  return await api.get(`/storys?t=${Date.now()}`);
+  return await api.get(`/storys?t=${Date.now()}`).catch(e => {
+    throw new Error(e);
+  });
 }
 
-export async function getStoryInfo(uuid: string) {
-  return await api.get(`/story/${uuid}?t=${Date.now()}`);
+// 获取单个故事详情
+export async function getStoryContent(uuid: string) {
+  return await api.get(`/story/${uuid}?t=${Date.now()}`).catch(e => {
+    throw new Error(e);
+  });
+}
+
+// 更新故事详情
+export async function updateStoryContent(uuid: string, content: Story) {
+  return await api.put(`/story/${uuid}`, content).catch(e => {
+    throw new Error(e);
+  });
+}
+
+// 删除故事
+export async function deleteStory(uuid: string) {
+  return await api.delete(`/story/${uuid}`).catch(e => {
+    throw new Error(e);
+  });
 }
